bcee4e43f29a14dd602fdf9fbae324bf9c802381
0811eljo-dot <0811eljo@skola.falkoping.se>
2025-08-21 09:38:46 +0000

    initial project setup with basic input handling and configuration files

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿Console.WriteLine("Skriv in ett heltal");
+string inm1 = Console.ReadLine();
+Console.WriteLine("Skriv in ett annat heltal");
+string inm2 = Console.ReadLine();
+
+// int tal1 = Convert.ToInt32(inm1);
+// int tal2 = Convert.ToInt32(inm2);
+
+// int summa = tal1 + tal2;
+
+// Console.WriteLine($"{tal1} {tal2} = {summa}");
+
+// int diff = tal1 - tal2;
+// Console.WriteLine($"{tal1} - {tal2} - {diff}");
+
+// int multiplication = tal1 * tal2;
+// Console.WriteLine($"{tal1} * {tal2} * {multiplication}");
+
+// int division = tal1 * tal2;
+// Console.WriteLine($"{tal1} / {tal2} / {division}");
+
+//dubble -> double floating point number 
+
+// double tal1 = Convert.ToDouble(inm1);
+// double tal2 = Convert.ToDouble(inm2);
+
+// double div = tal1 / tal2;
+
+// Console.WriteLine($"{tal1} / {tal2} = {div}");
+
+// double plus = tal1 + tal2;
+
+// Console.WriteLine($"{tal1} + {tal2} = {plus}");
+
+// double multi = tal1 * tal2;
+
+// Console.WriteLine($"{tal1} * {tal2} = {multi}");
+
+// double minus = tal1 - tal2;
+
+// Console.WriteLine($"{tal1} - {tal2} = {minus}");
+
+
+
+
+
+// // //Skapa en int
+// // int tal = 5;
+// // Console.WriteLine(tal);
+
+// // //Fel kod 
+// // Console.WriteLine("Hur gammal är du?");
+// // string tal1 = Console.ReadLine();
+
+// // Console.WriteLine("Hur gammal är din kompis?");
+// // string tal2 = Console.ReadLine();
+
+// // Console.WriteLine(tal1 + tal2);
+
+// //Rätt kod
+// Console.WriteLine("Hur gammal är du på riktigt?");
+// string inmatning = Console.ReadLine();
+// int talKonv = Convert.ToInt32(inmatning);
+
+// if (talKonv > 17)
+// {
+//     Console.WriteLine("Välkommen till krogen");
+// }
+
+// // Console.WriteLine("Välkommen!\nVad heter du?");
+
+// // string namn = Console.ReadLine();
+
+// // Console.WriteLine("Hej " + namn);
+// // Console.WriteLine("Vart bor du?");
+// // string plats = Console.ReadLine();
+
+// // Console.WriteLine("Vad gillar du att se på netflix?");
+// // string netflix = Console.ReadLine();
+
+// // Console.WriteLine($"Välkommen {namn}. Jag ser att du bor i/på {plats}.\n"
+// // + $"Ikväll tycker jag du ska kolla på {netflix}"); 
